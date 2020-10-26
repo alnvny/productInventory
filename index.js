ca7ce6265641af5017fd24fb408ec827b131b619
Gnana Allan Whinney Gnanapragasam <gnana.allan@oracle.com>
2020-10-27 02:17:37 +0530

    product in req body is mandatory

--- index.js
+++ index.js
@@ -52,26 +52,29 @@ app.get('/productInventory/:id', (req, res) => {
 
 app.put('/productInventory', (req, res) => {
     try {
-    reqProducts = req.body.product;
-    let isRequestBodyValid = validateRequstBody(reqProducts);
-    if (!isRequestBodyValid.success) {
-        return res.status(isRequestBodyValid.statusCode).json({ error: `bad request`, message: isRequestBodyValid.errMessage });
-    }
-
-    reqProductsObj = mergeDuplicateID(reqProducts);
-    if (!req.body.giveAllInStock) {
-        let isAllInStock = validateAllInStock(reqProductsObj, productInventoryDB);
-        if (!isAllInStock.success) {
-            return res.status(isAllInStock.statusCode).json({ error: `one of more item(s) out of stock`, errorMessage: isAllInStock.errorMsg })
+        reqProducts = req.body.product;
+        if (!reqProducts || reqProducts.length === 0) {
+            return res.status(400).json({ error: `bad request`, message: [`product array is mandatory in the request`] });
         }
-    }
-    let reservedObj = reserveStocks(reqProductsObj, productInventoryDB);
-    let responseMsg = createResponseMsg(reservedObj);
-    if (responseMsg.success) {
-        return res.status(responseMsg.statusCode).json({ success: responseMsg.success, message: responseMsg.message });
-    } else {
-        return res.status(responseMsg.statusCode).json({ error: "one of more item(s) out of stock ", message: responseMsg.message });
-    }
+        let isRequestBodyValid = validateRequstBody(reqProducts);
+        if (!isRequestBodyValid.success) {
+            return res.status(isRequestBodyValid.statusCode).json({ error: `bad request`, message: isRequestBodyValid.errMessage });
+        }
+
+        reqProductsObj = mergeDuplicateID(reqProducts);
+        if (!req.body.giveAllInStock) {
+            let isAllInStock = validateAllInStock(reqProductsObj, productInventoryDB);
+            if (!isAllInStock.success) {
+                return res.status(isAllInStock.statusCode).json({ error: `one of more item(s) out of stock`, errorMessage: isAllInStock.errorMsg })
+            }
+        }
+        let reservedObj = reserveStocks(reqProductsObj, productInventoryDB);
+        let responseMsg = createResponseMsg(reservedObj);
+        if (responseMsg.success) {
+            return res.status(responseMsg.statusCode).json({ success: responseMsg.success, message: responseMsg.message });
+        } else {
+            return res.status(responseMsg.statusCode).json({ error: "one of more item(s) out of stock ", message: responseMsg.message });
+        }
     }
     catch (error) {
         return res.status(500).json({ error: `Internal server errror`, message: [error] });
